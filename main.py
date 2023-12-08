8cbea8aac68cfb1c62dbfa906e77d51d158880f7
Hazyshades <30349843+Hazyshades@users.noreply.github.com>
2023-12-08 13:03:08 +0300

    switch driver called

--- main.py
+++ main.py
@@ -14,36 +14,36 @@ def login():
     chrome_options.add_argument('--profile-directory=Profile 1')
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    w = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe", chrome_options=chrome_options)
     time.sleep(5)
-    w.get(
+    driver.get(
         'https://www.app.rage.trade/vaults/0xa237af5e'
     )
     time.sleep(4)
 
-    w.find_element(By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div/header/div[2]/button[2]/div').click()
-    w.find_element(By.XPATH, '//*[@id="radix-:R1jlj6:"]/div[3]/div/button[1]').click()
+    driver.find_element(By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div/header/div[2]/button[2]/div').click()
+    driver.find_element(By.XPATH, '//*[@id="radix-:R1jlj6:"]/div[3]/div/button[1]').click()
     time.sleep(1)
-    w.switch_to.window(w.window_handles[1])
+    driver.switch_to.window(driver.window_handles[1])
     time.sleep(1)
-    # print("Second window title = " + w.title)
+    # print("Second window title = " + driver.title)
     time.sleep(1)
-    password = w.find_element(By.XPATH, '//*[@id="password"]')
+    password = driver.find_element(By.XPATH, '//*[@id="password"]')
     password.send_keys('test')  # set your password
     time.sleep(3)
     password.send_keys(Keys.ENTER)
     time.sleep(3)
     # check if button "Got It" exist
-    gotIt = w.find_element(By.XPATH, '//*[@id="popover-content"]/div/div/section/div[3]/button')
+    gotIt = driver.find_element(By.XPATH, '//*[@id="popover-content"]/div/div/section/div[3]/button')
 
     if gotIt != '':
         gotIt.click()
     time.sleep(3)
 
-    w.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/div[2]/button[2]').click()
     time.sleep(3)
 
-    w.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
     time.sleep(15)
 
 
